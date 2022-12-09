2b8fea43cfde7af65180e1d0cb28a2369cb55f0d
ilker <ilkerkaran@gmail.com>
2022-12-09 16:49:34 +0000

    day-9

--- solutions/day-9/firstSolution.js
+++ solutions/day-9/firstSolution.js
@@ -1,41 +1,12 @@
-import { headCoordinates } from './utils.js'
+import { getCoordinatesByLead, headCoordinates } from './utils.js'
 
 export default (arr) => {
   const headCoors = headCoordinates(arr)
-  let x = 0; let y = 0
   const visitedMap = {}
-  for (let i = 0; i < headCoors.length; i++) {
-    const [headX, headY] = headCoors[i].split(' ')
-    if (headX > x + 1) {
-      x++
-      if (headY > y) {
-        y++
-      } else if (headY < y) {
-        y--
-      }
-    } else if (headX < x - 1) {
-      x--
-      if (headY > y) {
-        y++
-      } else if ((headY < y)) {
-        y--
-      }
-    } else if (headY > y + 1) {
-      y++
-      if (headX > x) {
-        x++
-      } else if (headX < x) {
-        x--
-      }
-    } else if (headY < y - 1) {
-      y--
-      if (headX > x) {
-        x++
-      } else if (headX < x) {
-        x--
-      }
-    }
-    visitedMap[`${x} ${y}`] = true
+  const coors = getCoordinatesByLead(headCoors)
+  for (let i = 0; i < coors.length; i++) {
+    const element = coors[i]
+    visitedMap[element] = true
   }
   return Object.keys(visitedMap).length
 }
